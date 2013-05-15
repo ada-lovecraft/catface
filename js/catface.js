263fb26b34b60b8c276eeaec369c8485208a5cba
Jeremy Dowell <jdowell@fossil.com>
2013-05-15 16:30:50 -0500

    lol

--- js/catface.js
+++ js/catface.js
@@ -1,3 +1,16 @@
+$.fn.highlight = function(rect, color) {
+	$("<div />", {
+		"css": {
+			"border":   "2px solid " + color,
+			"position":	"absolute",
+			"left":		($(this).offset().left + rect[0]) + "px",
+			"top":		($(this).offset().top  + rect[1]) + "px",
+			"width": 	rect[2] + "px",
+			"height": 	rect[3] + "px"
+		}
+	}).appendTo("body");
+}
+
 $.getScript("http://static.codevinsky.com/catface/js/objectdetect.min.js", function(data, textStatus, jqxhr) {
 	images = $('img');
 	console.log(images);
@@ -5,6 +18,7 @@ $.getScript("http://static.codevinsky.com/catface/js/objectdetect.min.js", funct
 	$.each(images,function(index,image,array) {
 		$(images).objectdetect("all", {classifier: objectdetect.frontalface}, function(faces) {
 			console.log('images scanned....');
+			console.log('faces found: ' + faces.length);
 			for (var i = 0; i < faces.length; ++i) {
 				$(this).highlight(faces[i], "red");
 			}
