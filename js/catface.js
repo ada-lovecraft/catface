fa3d8213b56b72698fbc740cb896891c7c5ad6ad
Jeremy Dowell <jdowell@fossil.com>
2013-05-15 16:23:47 -0500

    lol

--- js/catface.js
+++ js/catface.js
@@ -1,8 +1,12 @@
 $.getScript("http://static.codevinsky.com/catface/js/objectdetect.min.js", function(data, textStatus, jqxhr) {
-	$("#face").objectdetect("all", {classifier: objectdetect.frontalface}, function(faces) {
-		for (var i = 0; i < faces.length; ++i) {
-			$(this).highlight(faces[i], "red");
-
-		}
+	images = $('img');
+	console.log(images.length + ' images found');
+	images.forEach(function(image,index,array) {
+		$(images).objectdetect("all", {classifier: objectdetect.frontalface}, function(faces) {
+			console.log('images scanned....');
+			for (var i = 0; i < faces.length; ++i) {
+				$(this).highlight(faces[i], "red");
+			}
+		});
 	});
 });
